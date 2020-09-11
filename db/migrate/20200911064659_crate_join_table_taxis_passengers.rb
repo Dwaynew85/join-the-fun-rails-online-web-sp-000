de1e2f1ad2e9ce722a473827066fec1e58d23d30
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-09-11 06:50:14 +0000

    Automatically backed up by Learn

--- db/migrate/20200911064659_crate_join_table_taxis_passengers.rb
+++
@@ -1,8 +0,0 @@
-class CrateJoinTableTaxisPassengers < ActiveRecord::Migration[5.0]
-  def change
-    create_join_table :taxis, :passengers do |t|
-      t.integer :taxi_id
-      t.integer :passenger_id
-    end
-  end
-end
