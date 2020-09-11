e518ed2441f1d538915fa2ea80f23d72b4255111
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-09-11 06:59:23 +0000

    Automatically backed up by Learn

--- db/migrate/20200911065354_add_foreign_key.rb
+++ db/migrate/20200911065354_add_foreign_key.rb
@@ -1,8 +1,6 @@
 class AddForeignKey < ActiveRecord::Migration[5.0]
   def change
-    add_column :rides do |t|
-      t.integer :passenger_id
-      t.integer :taxi_id
-    end
+    add_column :rides, :passenger_id, :integer 
+    add_column :rides, :taxi_id, :integer
   end
 end
